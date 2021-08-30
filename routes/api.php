a148ba49a5019c6b22969e3b5eb23d5c86e2b82f
Heycal14 <heycal30@gmail.com>
2021-08-30 19:29:02 +0700

    migration

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\BookController;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +18,11 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get('/Book', [BookController::class, 'index']);
+Route::post('/Book', [BookController::class, 'store']);
+Route::get('/Book/{id}', [BookController::class, 'show']);
+Route::put('/Book/{id}', [BookController::class, 'update']);
+Route::delete('/Book/{id}', [BookController::class, 'destroy']);
+
+// Route::resource('/books', BookController::class)->except('edit','create');
+

950c7e5c60d561e34a07c9065121d978bb3c21ed
Heycal14 <heycal30@gmail.com>
2021-09-23 22:25:01 +0700

    Challange Author

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\BookController;
+use App\Http\Controllers\AuthorController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,11 +19,19 @@ use App\Http\Controllers\BookController;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
-Route::get('/Book', [BookController::class, 'index']);
-Route::post('/Book', [BookController::class, 'store']);
-Route::get('/Book/{id}', [BookController::class, 'show']);
-Route::put('/Book/{id}', [BookController::class, 'update']);
-Route::delete('/Book/{id}', [BookController::class, 'destroy']);
+//Book
+// Route::get('/Book', [BookController::class, 'index']);
+// Route::post('/Book', [BookController::class, 'store']);
+// Route::get('/Book/{id}', [BookController::class, 'show']);
+// Route::put('/Book/{id}', [BookController::class, 'update']);
+// Route::delete('/Book/{id}', [BookController::class, 'destroy']);
 
-// Route::resource('/books', BookController::class)->except('edit','create');
+Route::resource('/books', BookController::class)->except('edit','create');
+
+//Author
+Route::get('/authors', [AuthorController::class, 'index']);
+Route::post('/authors', [AuthorController::class, 'store']);
+Route::get('/authors/{id}', [AuthorController::class, 'show']);
+Route::put('/authors/{id}', [AuthorController::class, 'update']);
+Route::delete('/authors/{id}', [AuthorController::class, 'destroy']);
 
